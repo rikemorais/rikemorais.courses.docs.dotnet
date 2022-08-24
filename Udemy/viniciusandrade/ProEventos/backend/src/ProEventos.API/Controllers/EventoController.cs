3fbb511172d71311fd2aae902584d9274d8aad80
Henrique Morais <rikeaju@hotmail.com>
2022-08-24 07:53:48 -0300

    Aula 013 - Evento Controller

---
+++ Udemy/viniciusandrade/ProEventos/backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(){}
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
